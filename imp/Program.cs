58bf3de039278fb5f12b53bca0611049200378e8
Benjamin Kowarsch <trijezdci@users.noreply.github.com>
2016-09-21 21:13:06 +0900

    new file

---
+++ imp/Program.cs
@@ -0,0 +1,256 @@
+﻿/* M2Sharp -- Modula-2 to C# Translator & Compiler
+ *
+ * Copyright (c) 2016 The Modula-2 Software Foundation
+ *
+ * Author & Maintainer: Benjamin Kowarsch <trijezdci@org.m2sf>
+ *
+ * @synopsis
+ *
+ * M2Sharp is a multi-dialect Modula-2 to C# translator and via-C# compiler.
+ * It supports the dialects described in the 3rd and 4th editions of Niklaus
+ * Wirth's book "Programming in Modula-2" (PIM) published by Springer Verlag,
+ * and an extended mode with select features from the revised language by
+ * B.Kowarsch and R.Sutcliffe "Modula-2 Revision 2010" (M2R10).
+ *
+ * In translator mode, M2Sharp translates Modula-2 source to C# source files.
+ * In compiler mode, M2Sharp compiles Modula-2 source via C# source files
+ * to object code or executables using the host system's C# compiler.
+ *
+ * @repository
+ *
+ * https://github.com/m2sf/m2sharp
+ *
+ * @file
+ *
+ * Program.cs
+ *
+ * Main program
+ *
+ * @license
+ *
+ * M2Sharp is free software: you can redistribute and/or modify it under the
+ * terms of the GNU Lesser General Public License (LGPL) either version 2.1
+ * or at your choice version 3 as published by the Free Software Foundation.
+ * However, you may not alter the copyright, author and license information.
+ *
+ * M2Sharp is distributed in the hope that it will be useful,  but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  Read the license for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with M2Sharp. If not, see <https://www.gnu.org/copyleft/lesser.html>.
+ *
+ * NB: Components in the domain part of email addresses are in reverse order.
+ */
+
+using System;
+
+namespace org.m2sf.m2sharp {
+
+class MainClass {
+
+/* ---------------------------------------------------------------------------
+ * method PrintBanner()
+ * ---------------------------------------------------------------------------
+ * Prints a banner to the console with name, version, copyright and license.
+ * ------------------------------------------------------------------------ */
+
+public static void PrintBanner () {
+      
+  Console.WriteLine("{0} version {1}.{2} ({3}), {4}",
+    ProductInfo.Name(),
+    ProductInfo.VersionMajor(), ProductInfo.VersionMinor(),
+    ProductInfo.Build(), ProductInfo.Description());
+
+  PrintCopyright();
+  PrintLicense();
+
+} /* end PrintBanner */
+
+
+/* ---------------------------------------------------------------------------
+ * constant HelpText
+ * ------------------------------------------------------------------------ */
+
+const string HelpText =
+  "usage:\n" +
+  " m2sharp info | compilation\n\n" +
+
+  "info:\n" +
+  " --help, -h      print help\n" +
+  " --version, -V   print version info\n" +
+  " --license       print license info\n\n" +
+
+  "compilation:\n" +
+  " [dialect] [diagnostics] [products] [capabilities] sourcefile\n\n" +
+
+  "dialect:\n" +
+  " --pim3   follow PIM, third edition\n" +
+  " --pim4   follow PIM, fourth edition\n" +
+  " --ext    use extended dialect mode (default)\n\n" +
+
+  "diagnostics:\n" +
+  " --verbose, -v         verbose mode\n" +
+  " --lexer-debug         lexer debug mode\n" +
+  " --parser-debug        parser debug mode\n" +
+  " --print-settings      print all settings\n" +
+  " --errant-semicolons   tolerate errant semicolons\n\n" +
+
+  "products:\n" +
+  " --ast, --no-ast       .ast file output\n" +
+  " --graph, --no-graph   .dot file output\n" +
+  " --xlat, --no-xlat     .cs file output\n" +
+  " --obj, --no-obj       .obj and .sym file output\n\n" +
+
+  " --syntax-only   syntax check only\n" +
+  " --ast-only      .ast file only output\n" +
+  " --graph-only    .dot file only output\n" +
+  " --xlat-only     .cs file only output\n" +
+  " --obj-only      .obj and .sym file only output\n\n" +
+
+  " in combination with --xlat:\n" +
+  " --preserve-comments   preserve comments in .cs files\n" +
+  " --strip-comments      strip comments from .cs files\n\n" +
+
+  "capabilities:\n\n" +
+
+  " in combination with --pim3 or --pim4:\n" +
+  " --compliant, --safer\n" +
+  " --synonyms, --no-synonyms\n" +
+  " --octal-literals, --no-octal-literals\n" +
+  " --explicit-cast, --no-explicit-cast\n" +
+  " --coroutines, --no-coroutines\n" +
+  " --variant-records, --no-variant-records\n" +
+  " --local-modules, --no-local-modules\n\n" +
+
+  " in combination with --ext:\n" +
+  " --lowline-identifiers, --no-lowline-identifiers\n" +
+  " --to-do-statement, --no-to-do-statement\n\n" +
+
+  "sourcefile:\n" +
+  " may include directory path\n" +
+  " must have suffix .def or .mod";
+
+
+/* ---------------------------------------------------------------------------
+ * method PrintHelp()
+ * ---------------------------------------------------------------------------
+ * Prints usage help to the console.
+ * ------------------------------------------------------------------------ */
+
+public static void PrintHelp () {
+  Console.WriteLine(HelpText);
+} /* end PrintHelp */
+
+
+/* ---------------------------------------------------------------------------
+ * method PrintVersion()
+ * ---------------------------------------------------------------------------
+ * Prints version info to the console.
+ * ------------------------------------------------------------------------ */
+
+public static void PrintVersion () {
+  Console.WriteLine("version {0}.{1} ({2})",
+    ProductInfo.VersionMajor(),
+    ProductInfo.VersionMinor(),
+    ProductInfo.Build());
+} /* end PrintVersion */
+    
+
+/* ---------------------------------------------------------------------------
+ * method PrintCopyright()
+ * ---------------------------------------------------------------------------
+ * Prints copyright info to the console.
+ * ------------------------------------------------------------------------ */
+
+public static void PrintCopyright () {
+  Console.WriteLine("copyright {0}", ProductInfo.Copyright());
+} /* end PrintCopyright */
+
+    
+/* ---------------------------------------------------------------------------
+ * method PrintLicense()
+ * ---------------------------------------------------------------------------
+ * Prints license info to the console.
+ * ------------------------------------------------------------------------ */
+
+public static void PrintLicense () {
+  Console.WriteLine("licensed under the {0}", ProductInfo.License());
+} /* end PrintLicense */
+
+
+/* ---------------------------------------------------------------------------
+ * method PrintOSInfo()
+ * ---------------------------------------------------------------------------
+ * Prints OS name and version info to the console.
+ * ------------------------------------------------------------------------ */
+
+public static void PrintOSInfo () {
+
+  if (PlatformInfo.IsSupported()) {
+    Console.WriteLine("running on {0}, {1} version {2}.{3} ({4})",
+      PlatformInfo.Name(),
+      PlatformInfo.Uname(),
+      PlatformInfo.VersionMajor(),
+      PlatformInfo.VersionMinor(), PlatformInfo.Build());
+  }
+  else /* unsupported platform */ {
+    Console.WriteLine("platform not supported");
+  } /* end if */
+
+} /* end PrintOSInfo */
+
+
+/* ---------------------------------------------------------------------------
+ * method Main(args)
+ * ---------------------------------------------------------------------------
+ * Starts the compiler.
+ * ------------------------------------------------------------------------ */
+    
+public static void Main (string[] args) {
+  ArgumentStatus status;
+
+  if (PlatformInfo.IsSupported() == false) {
+    Console.WriteLine("platform not supported");
+    Environment.Exit(1);
+  } /* end if */
+
+  ArgumentParser.ParseOptions(args);
+
+  switch (x) {
+
+    case HelpRequested :
+      PrintHelp();
+      Environment.Exit(0);
+      break;
+
+    case VersionRequested :
+      PrintVersion();
+      Environment.Exit(0);
+      break;
+
+    case LicenseRequested :
+      PrintLicense();
+      Environment.Exit(0);
+      break;
+
+    case CompilationRequested :
+      PrintBanner();
+      break;
+
+    default :
+      Console.WriteLine("use m2sharp --help for usage info");
+      Environment.Exit(1);
+      break;
+
+  } /* end switch */
+
+  // TO DO : call parser on input file
+
+} /* end Main */
+
+} /* MainClass */
+
+} /* namespace */
+
+/* END OF FILE */
